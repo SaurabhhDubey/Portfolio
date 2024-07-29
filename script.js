352b4131181bee10cacc08943dc943cb7c392ed1
Saurabh Dubey <155214617+SaurabhhDubey@users.noreply.github.com>
2024-07-29 13:12:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+    strings: ['Web Devloper', 'Graphic Designor', 'Video Editor'],
+    typeSpeed: 50,
+});
+const btn = document.getElementById('btnn');
+btn.addEventListener('click', function(){
+    const url = 'https://drive.google.com/file/d/10GZ8XyXiHv6huiST6eh4CqYxmvKk-KGA/view?usp=drivesdk';
+    window.open(url,'');
+});
+
