d6dbde1897a44153f8e39b78247983c65b056287
Saurabh Dubey <saurabhdubey3712@gmail.com>
2024-08-01 09:31:59 +0530

    chnanges in js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var typed = new Typed('#element', {
     strings: ['Web Devloper', 'Graphic Designor', 'Video Editor'],
     typeSpeed: 50,
@@ -9,15 +9,4 @@ btn.addEventListener('click', function(){
     window.open(url,'');
 });
 
-=======
-var typed = new Typed('#element', {
-    strings: ['Web Devloper', 'Graphic Designor', 'Video Editor'],
-    typeSpeed: 50,
-});
-const btn = document.getElementById('btnn');
-btn.addEventListener('click', function(){
-    const url = 'https://drive.google.com/file/d/10GZ8XyXiHv6huiST6eh4CqYxmvKk-KGA/view?usp=drivesdk';
-    window.open(url,'');
-});
 
->>>>>>> 352b4131181bee10cacc08943dc943cb7c392ed1
